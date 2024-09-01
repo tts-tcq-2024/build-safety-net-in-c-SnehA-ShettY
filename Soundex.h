1b1ff76f21430856aaa970f8321f9bb6d7afb81d
SnehA-ShettY <95692157+SnehA-ShettY@users.noreply.github.com>
2024-09-01 17:16:09 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,25 +4,25 @@
 #include <ctype.h>
 #include <string.h>
 
-// Lookup table to simplify the mapping
-const char soundexTable[26] = {
-    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
-};
-
 char getSoundexCode(char c) {
     c = toupper(c);
-    if (c >= 'A' && c <= 'Z') {
-        return soundexTable[c - 'A'];
-    }
-    return '0'; // For characters outside A-Z
+    const char *groups = "BFPVCGJKQSXZDTLMNR";
+    const char *codes  = "111122222222334556";
+    
+    const char *pos = strchr(groups, c);
+    return pos ? codes[pos - groups] : '0';
 }
 
 void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
+    if (!name || !*name) {
+        soundex[0] = '\0';
+        return;
+    }
+
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
+    for (int i = 1; name[i] != '\0' && sIndex < 4; ++i) {
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != soundex[sIndex - 1]) {
             soundex[sIndex++] = code;
