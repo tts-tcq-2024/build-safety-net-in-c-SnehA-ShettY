7761c25199e282f6587c1fd31484a9f7d89fecec
SnehA-ShettY <95692157+SnehA-ShettY@users.noreply.github.com>
2024-09-02 19:01:16 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,18 +1,22 @@
 #include <gtest/gtest.h>
-#include <cstring>
 #include "Soundex.h"
 
-// Test getSoundexCode function
+// Test getSoundexCode function for all branches
 TEST(SoundexTest, GetSoundexCode) {
+    // Test valid letters
     EXPECT_EQ(getSoundexCode('A'), '0');
     EXPECT_EQ(getSoundexCode('B'), '1');
     EXPECT_EQ(getSoundexCode('C'), '2');
-    EXPECT_EQ(getSoundexCode('D'), '3');
-    EXPECT_EQ(getSoundexCode('E'), '0');
-    EXPECT_EQ(getSoundexCode('F'), '1');
-    EXPECT_EQ(getSoundexCode('G'), '2');
-    EXPECT_EQ(getSoundexCode('H'), '0');
     EXPECT_EQ(getSoundexCode('Z'), '2');
+
+    // Test lowercase letters to check conversion to uppercase
+    EXPECT_EQ(getSoundexCode('a'), '0');
+    EXPECT_EQ(getSoundexCode('b'), '1');
+
+    // Test non-alphabet characters
+    EXPECT_EQ(getSoundexCode('1'), '0');
+    EXPECT_EQ(getSoundexCode('@'), '0');
+    EXPECT_EQ(getSoundexCode(' '), '0');
 }
 
 // Test fetch_firstchar function
@@ -22,21 +26,29 @@ TEST(SoundexTest, FetchFirstChar) {
     EXPECT_EQ(fetch_firstchar(""), '\0');
 }
 
-// Test appendSoundex function
+// Test appendSoundex function for all branches
 TEST(SoundexTest, AppendSoundex) {
     char soundex[5] = "A";
     char prevCode = '1';
     int length = 1;
 
+    // Test appending different code
     appendSoundex(soundex, '2', &prevCode, &length);
     EXPECT_STREQ(soundex, "A2");
     EXPECT_EQ(length, 2);
 
-    appendSoundex(soundex, '3', &prevCode, &length);
-    EXPECT_STREQ(soundex, "A23");
-    EXPECT_EQ(length, 3);
+    // Test appending the same code (shouldn't append)
+    appendSoundex(soundex, '2', &prevCode, &length);
+    EXPECT_STREQ(soundex, "A2");
+    EXPECT_EQ(length, 2);
 
-    appendSoundex(soundex, '3', &prevCode, &length);  // Should not append same code
+    // Test appending '0' code (shouldn't append)
+    appendSoundex(soundex, '0', &prevCode, &length);
+    EXPECT_STREQ(soundex, "A2");
+    EXPECT_EQ(length, 2);
+
+    // Test appending another different code
+    appendSoundex(soundex, '3', &prevCode, &length);
     EXPECT_STREQ(soundex, "A23");
     EXPECT_EQ(length, 3);
 }
@@ -46,22 +58,39 @@ TEST(SoundexTest, InitializeSoundex) {
     char soundex[5] = "";
     int length = 0;
 
+    // Test with valid name
     initializeSoundex("Smith", 'S', soundex, &length);
     EXPECT_STREQ(soundex, "S5");
     EXPECT_EQ(length, 2);
+
+    // Test with name starting with 'A' (Soundex '0' should not append)
+    initializeSoundex("Adams", 'A', soundex, &length);
+    EXPECT_STREQ(soundex, "A");
+    EXPECT_EQ(length, 1);
 }
 
-// Test processSoundex function
+// Test processSoundex function for full coverage
 TEST(SoundexTest, ProcessSoundex) {
     char soundex[5] = "";
     int length = 0;
 
+    // Test with a typical name
     processSoundex("Smith", 'S', soundex, &length);
     EXPECT_STREQ(soundex, "S53");
     EXPECT_EQ(length, 3);
+
+    // Test with a short name that requires padding
+    processSoundex("Li", 'L', soundex, &length);
+    EXPECT_STREQ(soundex, "L3");
+    EXPECT_EQ(length, 2);
+
+    // Test with a name where all letters are the same Soundex code
+    processSoundex("Bbbb", 'B', soundex, &length);
+    EXPECT_STREQ(soundex, "B1");
+    EXPECT_EQ(length, 2);
 }
 
-// Test paddingSoundex function
+// Test paddingSoundex function for full coverage
 TEST(SoundexTest, PaddingSoundex) {
     char soundex[5] = "S5";
 
@@ -75,25 +104,45 @@ TEST(SoundexTest, PaddingSoundex) {
     strcpy(soundex, "S530");
     paddingSoundex(soundex);
     EXPECT_STREQ(soundex, "S530");
+
+    strcpy(soundex, "S5");
+    paddingSoundex(soundex);
+    EXPECT_STREQ(soundex, "S500");
 }
 
-// Test generateSoundex function
+// Test generateSoundex function for all scenarios
 TEST(SoundexTest, GenerateSoundex) {
     char soundex[5] = "";
 
+    // Test typical names
     generateSoundex("Smith", soundex);
     EXPECT_STREQ(soundex, "S530");
 
     generateSoundex("Smythe", soundex);
     EXPECT_STREQ(soundex, "S530");
 
+    // Test name requiring padding
+    generateSoundex("Li", soundex);
+    EXPECT_STREQ(soundex, "L300");
+
+    // Test name with non-alphabet characters
+    generateSoundex("O'Brien", soundex);
+    EXPECT_STREQ(soundex, "O165");
+
+    // Test name starting with 'A' and checking padding
     generateSoundex("Ashcraft", soundex);
     EXPECT_STREQ(soundex, "A261");
 
+    // Test name with repeating Soundex codes
     generateSoundex("Tymczak", soundex);
-    EXPECT_STREQ(soundex, "T520");
+    EXPECT_STREQ(soundex, "T522");
 
+    // Test empty string
     generateSoundex("", soundex);
     EXPECT_STREQ(soundex, "");
 }
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
