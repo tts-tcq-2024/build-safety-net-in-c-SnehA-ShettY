a0fa8dcf7dd4f257ded04daaef72633f7d6cf589
SnehA-ShettY <95692157+SnehA-ShettY@users.noreply.github.com>
2024-09-03 08:40:35 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,22 +4,22 @@
 
 // Test getSoundexCode function
 TEST(SoundexTest, GetSoundexCode) {
-    EXPECT_EQ(getSoundexCode('A'), '0');
-    EXPECT_EQ(getSoundexCode('B'), '1');
-    EXPECT_EQ(getSoundexCode('C'), '2');
-    EXPECT_EQ(getSoundexCode('D'), '3');
-    EXPECT_EQ(getSoundexCode('E'), '0');
-    EXPECT_EQ(getSoundexCode('F'), '1');
-    EXPECT_EQ(getSoundexCode('G'), '2');
-    EXPECT_EQ(getSoundexCode('H'), '0');
-    EXPECT_EQ(getSoundexCode('Z'), '2');
+    EXPECT_EQ(get_Soundex_Code('A'), '0');
+    EXPECT_EQ(get_Soundex_Code('B'), '1');
+    EXPECT_EQ(get_Soundex_Code('C'), '2');
+    EXPECT_EQ(get_Soundex_Code('D'), '3');
+    EXPECT_EQ(get_Soundex_Code('E'), '0');
+    EXPECT_EQ(get_Soundex_Code('F'), '1');
+    EXPECT_EQ(get_Soundex_Code('G'), '2');
+    EXPECT_EQ(get_Soundex_Code('H'), '0');
+    EXPECT_EQ(get_Soundex_Code('Z'), '2');
 }
 
 // Test fetch_firstchar function
 TEST(SoundexTest, FetchFirstChar) {
-    EXPECT_EQ(fetch_firstchar("Example"), 'E');
-    EXPECT_EQ(fetch_firstchar("soundex"), 'S');
-    EXPECT_EQ(fetch_firstchar(""), '\0');
+    EXPECT_EQ(fetch_first_char("Example"), 'E');
+    EXPECT_EQ(fetch_first_char("soundex"), 'S');
+    EXPECT_EQ(fetch_first_char(""), '\0');
 }
 
 // Test appendSoundex function
@@ -28,15 +28,15 @@ TEST(SoundexTest, AppendSoundex) {
     char prevCode = '1';
     int length = 1;
 
-    appendSoundex(soundex, '2', &prevCode, &length);
+    append_Soundex(soundex, '2', &prevCode, &length);
     EXPECT_STREQ(soundex, "A2");
     EXPECT_EQ(length, 2);
 
-    appendSoundex(soundex, '3', &prevCode, &length);
+    append_Soundex(soundex, '3', &prevCode, &length);
     EXPECT_STREQ(soundex, "A23");
     EXPECT_EQ(length, 3);
 
-    appendSoundex(soundex, '3', &prevCode, &length);  // Should not append same code
+    append_Soundex(soundex, '3', &prevCode, &length);  // Should not append same code
     EXPECT_STREQ(soundex, "A23");
     EXPECT_EQ(length, 3);
 }
@@ -46,7 +46,7 @@ TEST(SoundexTest, InitializeSoundex) {
     char soundex[5] = "";
     int length = 0;
 
-    initializeSoundex("Smith", 'S', soundex, &length);
+    initialize_Soundex("Smith", 'S', soundex, &length);
     EXPECT_STREQ(soundex, "S5");
     EXPECT_EQ(length, 2);
 }
@@ -56,7 +56,7 @@ TEST(SoundexTest, ProcessSoundex) {
     char soundex[5] = "";
     int length = 0;
 
-    processSoundex("Smith", 'S', soundex, &length);
+    process_Soundex("Smith", 'S', soundex, &length);
     EXPECT_STREQ(soundex, "S53");
     EXPECT_EQ(length, 3);
 }
@@ -65,15 +65,15 @@ TEST(SoundexTest, ProcessSoundex) {
 TEST(SoundexTest, PaddingSoundex) {
     char soundex[5] = "S5";
 
-    paddingSoundex(soundex);
+    padding_Soundex(soundex);
     EXPECT_STREQ(soundex, "S500");
 
     strcpy(soundex, "S53");
-    paddingSoundex(soundex);
+    padding_Soundex(soundex);
     EXPECT_STREQ(soundex, "S530");
 
     strcpy(soundex, "S530");
-    paddingSoundex(soundex);
+    padding_Soundex(soundex);
     EXPECT_STREQ(soundex, "S530");
 }
 
@@ -81,18 +81,18 @@ TEST(SoundexTest, PaddingSoundex) {
 TEST(SoundexTest, GenerateSoundex) {
     char soundex[5] = "";
 
-    generateSoundex("Smith", soundex);
+    generate_Soundex("Smith", soundex);
     EXPECT_STREQ(soundex, "S530");
 
-    generateSoundex("Smythe", soundex);
+    generate_Soundex("Smythe", soundex);
     EXPECT_STREQ(soundex, "S530");
 
-    generateSoundex("Ashcraft", soundex);
+    generate_Soundex("Ashcraft", soundex);
     EXPECT_STREQ(soundex, "A261");
 
-    generateSoundex("Tymczak", soundex);
+    generate_Soundex("Tymczak", soundex);
     EXPECT_STREQ(soundex, "T520");
 
-    generateSoundex("", soundex);
+    generate_Soundex("", soundex);
     EXPECT_STREQ(soundex, "");
 }
